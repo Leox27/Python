96e43e2d1d3158a0e913941fd1f972677b1b140d
Leox27 <mayurx27@gmail.com>
2026-07-17 18:47:08 +0530

    Solved Que2: Instance Methods

--- Practice Question/OOPs/01_Inheritance/01_classes_objects_&_constructor.py
+++ Practice Question/OOPs/01_Inheritance/01_classes_objects_&_constructor.py
@@ -48,3 +48,69 @@ Marks: 76
 '''
 
 
+"""
+Question 2 (Instance Methods)
+
+Create a class named Student2.
+
+Requirements
+Use the __init__() constructor.
+The class should have these instance variables:
+- name
+- roll_no
+- marks
+
+Create a method named display() that prints all the student's details.
+Create another method named is_pass().
+If marks are 40 or more, print:
+"<name> has Passed"
+Otherwise, print:
+"<name> has Failed"
+Create 3 student objects.
+At least one student should have marks less than 40.
+Call both methods for every object.
+"""
+
+class Student2():
+    
+    def __init__(self, name, rollno, marks):
+        self.name = name
+        self.rollno = rollno
+        self.marks = marks
+        
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Roll No.: {self.rollno}")
+        print(f"Name: {self.marks}")
+    
+    def is_pass(self):
+        if self.marks >= 40:
+            print(f"{self.name} is Passed")
+        else:
+            print(f"{self.name} is Failed")
+
+student4 = Student2("Suraj", 101, 70)
+student5 = Student2("kashinath", 101, 35)
+student6 = Student2("Boomer", 101, 50)
+
+student4.display()
+student4.is_pass()
+student5.display()
+student5.is_pass()
+student6.display()
+student6.is_pass()
+
+'''
+Name: Suraj
+Roll No.: 101
+Name: 70
+Suraj is Passed
+Name: kashinath
+Roll No.: 101
+Name: 35
+kashinath is Failed
+Name: Boomer
+Roll No.: 101
+Name: 50
+Boomer is Passed
+'''
